0798fcb3400df8a0813cbf462e1a3928aff3ce85
Jatin <jatin@foyer.work>
2025-01-29 17:40:50 +0530

    feat: save to merlin activity

--- lib/model/sharing_file.dart
+++ lib/model/sharing_file.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 // ignore_for_file: constant_identifier_names
 
 class SharedFile {
@@ -14,22 +15,49 @@ class SharedFile {
   /// Whether its a video or image or file
   SharedMediaType type = SharedMediaType.OTHER;
 
-  SharedFile(
-      {required this.value,
-      this.thumbnail,
-      this.duration,
-      this.type = SharedMediaType.OTHER});
+  /// Action type
+  ActionType action = ActionType.ASK;
+
+  /// Whether its a context menu
+  bool isContextMenu = false;
+
+  SharedFile({
+    required this.value,
+    this.thumbnail,
+    this.duration,
+    this.type = SharedMediaType.OTHER,
+    this.action = ActionType.ASK,
+    this.isContextMenu = false,
+  });
 
   SharedFile.fromJson(Map<String, dynamic> json)
       : value = json['value'] ?? json['path'],
         thumbnail = json['thumbnail'],
         duration = json['duration'],
-        type = SharedMediaType.values[json['type']];
+        type = SharedMediaType.values[json['type']],
+        action = ActionType.values.byName(json['action']),
+        isContextMenu = json['isContextMenu'] ?? false;
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'value': value,
+      'thumbnail': thumbnail,
+      'duration': duration,
+      'type': type.name,
+      'action': action.name,
+      'isContextMenu': isContextMenu,
+    };
+  }
 
   @override
   String toString() {
-    return "{ value:$value, thumbnail:$thumbnail, duration:$duration, type:$type }";
+    return 'SharedFile(value: $value, thumbnail: $thumbnail, duration: $duration, type: $type, action: $action, isContextMenu: $isContextMenu)';
   }
 }
 
 enum SharedMediaType { TEXT, URL, IMAGE, VIDEO, FILE, OTHER }
+
+enum ActionType {
+  ASK,
+  SAVE;
+}
